e1ac4a7f1d19101c4ae3463d42f5e5f2591fc637
Daniel <danielps99@gmail.com>
2020-11-25 22:41:14 -0300

    Start Spring Project

---
+++ src/test/java/br/com/br/startSpring/StartSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.br.startSpring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StartSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
