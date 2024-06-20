b01c2857bc44510cca7ada19e8d53f3ccce3672f
Daniel Pereira dos Santos <danielps99@gmail.com>
2024-06-20 15:11:18 -0300

    feature: Show app version in swagger page /swagger-ui/index.html  Changes to be committed: 	modified:   src/main/java/br/com/bdws/startSpring/config/swagger/SwaggerConfiguration.java 	modified:   src/main/resources/application-dev.properties 	modified:   src/main/resources/application-devdocker.properties 	modified:   src/main/resources/application-test.properties 	modified:   src/main/resources/application.properties

--- src/main/java/br/com/bdws/startSpring/config/swagger/SwaggerConfiguration.java
+++ src/main/java/br/com/bdws/startSpring/config/swagger/SwaggerConfiguration.java
@@ -6,6 +6,7 @@ import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.security.SecurityRequirement;
 import io.swagger.v3.oas.models.security.SecurityScheme;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -14,12 +15,15 @@ import java.util.Arrays;
 @Configuration
 public class SwaggerConfiguration {
 
+    @Value("${app-version}")
+    private String appVersion;
+
     @Bean
     public OpenAPI customCofiguration() {
         return new OpenAPI()
             .info(new Info().title("Start Spring")
                 .description("Start Spring Study Project API")
-                .version("1.0"))
+                .version(appVersion))
             .externalDocs(new ExternalDocumentation()
                 .description("API Documentation"))
             .components(new Components().addSecuritySchemes("bearer-jwt",
